fcc6cc61a26d5d34decd8309fab75c8cf9f0b252
KangByung wook <byungwook@KangByungui-MacBook-Pro.local>
2016-05-01 21:48:10 +0900

    first commit

---
+++ src/oodp/android_sample_1/MainActivity.java
@@ -0,0 +1,63 @@
+package oodp.android_sample_1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		int id = item.getItemId();
+		if ( id == R.id.action_settings )
+		{
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/*
+	 * 자동으로 생성된 코드의 끝
+	 * ---------------------------------------------------------------------------------
+	 */
+	
+	public void bt_people_Click(View view)
+	{
+		/*
+		 * 이 Activity에서 별다른 처리 없이 다른 Activity로 전이할 때 사용할 수 있는 코드입니다.
+		 */
+		Intent peopleActivity = new Intent(this, PeopleActivity.class);
+		startActivity(peopleActivity);
+	}
+	
+	public void bt_reset_Click(View view)
+	{
+		/*
+		 * 이전에 기록된 DB의 내용을 초기화하고 싶을 때 사용할 수 있는 코드입니다.
+		 */
+		DBHelper helper = new DBHelper(this);
+		SQLiteDatabase db = helper.getWritableDatabase();
+		helper.onUpgrade(db, 1, 1);
+	}
+}
---
+++ src/oodp/android_sample_1/NewPeopleActivity.java
@@ -0,0 +1,76 @@
+package oodp.android_sample_1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class NewPeopleActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new_people);
+		
+		Intent intent = getIntent();
+
+		/*
+		 * intent로 전달받은 문자열을 통해 EditText의 초기 문자열을 설정하는 코드입니다.
+		 */
+		EditText tb_name = (EditText)findViewById(R.id.tb_name);
+		EditText tb_email = (EditText)findViewById(R.id.tb_number);
+		EditText tb_group = (EditText)findViewById(R.id.tb_group);
+		
+		
+		tb_name.setText(intent.getStringExtra("name"));
+		tb_email.setText(intent.getStringExtra("number"));
+		tb_group.setText(intent.getStringExtra("group"));
+		
+		
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		getMenuInflater().inflate(R.menu.new_people, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		int id = item.getItemId();
+		if ( id == R.id.action_settings )
+		{
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void bt_accept_Click(View view)
+	{
+		Intent intent = new Intent();
+		
+		/*
+		 * EditText로 입력받은 문자열을 전달하기 위해 intent에 담는 코드입니다.
+		 */
+		EditText tb_name = (EditText)findViewById(R.id.tb_name);
+		EditText tb_email = (EditText)findViewById(R.id.tb_number);
+		
+		intent.putExtra("name", tb_name.getText().toString());
+		intent.putExtra("email", tb_email.getText().toString());
+		
+		/*
+		 * 이 Activity의 작업을 성공적으로 종료하고 이전 Activity로 돌아가기 위한 코드입니다.
+		 */
+		setResult(RESULT_OK, intent);
+		finish();
+	}
+}
